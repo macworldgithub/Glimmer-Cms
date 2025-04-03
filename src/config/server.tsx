95a1322d71267dfe7493eb67b65d638c6ebf0d66
hammad lodhi <hammadak05@gmail.com>
2025-04-03 15:50:48 +0500

    fix

--- src/config/server.tsx
+++ src/config/server.tsx
@@ -1,7 +1,7 @@
 export const developmentServer = "http://localhost:3000";
 export const productionServer = "https://api.glimmer.com.pk";
 
-const curr_env: "Prod" | "DEV" = "DEV";
+const curr_env: "Prod" | "DEV" = "Prod";
 
 export const BACKEND_URL =
   //@ts-ignore
