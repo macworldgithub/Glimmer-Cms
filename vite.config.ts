72e4d44306af7bde000014cdda90952de1542a27
shweeta <shweeta@macworldtechnology.com>
2025-06-03 15:37:52 +0500

    chngeee

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
+
 export default defineConfig({
   plugins: [react()],
   css: {
     postcss: {
       plugins: [tailwindcss()],
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
     },
   },
 });
