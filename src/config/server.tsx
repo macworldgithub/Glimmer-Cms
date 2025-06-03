eb1a6fbce78deefcb72c85459b8201206a9ab102
shweeta <shweeta@macworldtechnology.com>
2025-06-03 15:24:19 +0500

    ...

--- src/config/server.tsx
+++ src/config/server.tsx
@@ -5,5 +5,5 @@ const curr_env: "Prod" | "DEV" = "Prod";
 
 // export const BACKEND_URL =
 //   //@ts-ignore
-//   curr_env === "DEV" ? developmentServer : productionServer;
-export const BACKEND_URL = "http://localhost:3000";
+curr_env === "Prod" ? developmentServer : productionServer;
+export const BACKEND_URL = "https://www.api.glimmer.com.pk";
